7bc168da5211a6cffb0e89c385c602025267a677
Peter Čagala <petercagala@gmail.com>
2023-04-29 07:15:00 +0200

    112-1: Rendering Logic in the App

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -13,6 +13,16 @@ class Fib extends Component {
         this.fetchIndexes();
     }
 
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        axios.post("/api/values", {
+            index: this.state.index,
+        });
+        this.setState({
+           index: "",
+        });
+    };
+
     async fetchValues() {
         const values =  await axios.get("/api/values/current");
         this.setState({
@@ -26,4 +36,47 @@ class Fib extends Component {
             seenIndexes: seenIndexes.data
         });
     }
+
+    renderSeeenIndexes() {
+        return this.state.seenIndexes.map((value) => {
+            return value;
+        }).join(", ");
+    }
+
+    renderValues() {
+        const entries = [];
+
+        for(let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key} I calculated {this.state.values[key]}
+                </div>
+            );
+        }
+        return entries;
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Enter your index:
+                    </label>
+                    <input
+                        value={this.state.index}
+                        onChange={event => this.setState({ index: event.target.value})}
+                    />
+                    <button>Submit</button>
+                </form>
+                <h3>Indexes I have seen:</h3>
+                {this.renderSeeenIndexes()}
+
+                <h3>Calculated values:</h3>
+                {this.renderValues()}
+            </div>
+        );
+    }
 }
+
+export default Fib;
